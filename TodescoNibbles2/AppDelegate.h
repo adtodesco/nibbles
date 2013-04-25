3d5c524a1191398d2587f68dece2f3fa7c3d04d2
Anthony Todesco <sportmaster65@dhcp-213-252.wireless.bowdoin.edu>
2013-04-25 14:15:12 -0400

    Initial Commit

---
+++ TodescoNibbles2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TodescoNibbles2
+//
+//  Created by Anthony Todesco on 4/25/13.
+//  Copyright (c) 2013 Anthony Todesco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TodescoNibbles2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TodescoNibbles2
+//
+//  Created by Anthony Todesco on 4/25/13.
+//  Copyright (c) 2013 Anthony Todesco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
